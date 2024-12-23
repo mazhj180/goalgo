9b232c275216de2f1da646d807d5628bd80315d5
mazhj <mazhj180@163.com>
2024-12-23 22:10:35 +0800

    :white_check_mark: 更新测试字典树功能

---
+++ data_structures/trees/test/tire_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"fmt"
+	"mazhj.com/goalgo/data_structures/trees"
+	"testing"
+)
+
+var data = []string{"你好", "你好呀", "你好吗", "你是谁", "我不在乎", "我不在意", "我和你"}
+
+func TestTire(t *testing.T) {
+	tire := trees.Tire{}
+	for _, v := range data {
+		tire.AddWord(v)
+	}
+	for _, v := range tire.Retrieve("你好") {
+		fmt.Println(v)
+	}
+}
